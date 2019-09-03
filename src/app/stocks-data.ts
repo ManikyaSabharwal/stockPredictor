fc81dac947fce73e693964c8336212b7110c7e28
ManikyaSabharwal <46395883+ManikyaSabharwal@users.noreply.github.com>
2019-09-04 00:54:18 +0530

    working info

--- src/app/stocks-data.ts
+++ src/app/stocks-data.ts
@@ -1,5 +1,5 @@
 import { Stock } from './stock';
 export const STOCKS: Stock[] = [
-  { symbol: 'INFY', prices: [10, 20, 30, 15, 100, 200, 180], name: "Infosys" },
-  { symbol: 'APPL', prices: [100, 200, 300, 180, 300, 310, 360], name: "Apple" }
+  { symbol: 'INFY', prices: [10, 20, 30, 15, 70, 85, 98], name: "Infosys" },
+  { symbol: 'APPL', prices: [10, 20, 30, 18, 30, 31, 36], name: "Apple" }
 ]
